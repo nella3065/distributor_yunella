182fc8c874bfe65d205395ef01f5250bb76b0e7c
nella3065 <yunella3065@gmail.com>
2026-05-02 16:47:51 +0700

    update project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
+import { ProductsModule } from './products/products.module';
+import { StockModule } from './stock/stock.module';
+import { ScheduleModule } from './schedule/schedule.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [AuthModule], // import modul autentikasi ke aplikasi utama
+  imports: [AuthModule, UsersModule, SuppliersModule, ProductsModule, StockModule, ScheduleModule, OrdersModule, PaymentsModule, TransactionsModule, ReportsModule], // import modul autentikasi ke aplikasi utama
 })
 export class AppModule {}
